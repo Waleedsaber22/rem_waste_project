f26aca9fda897c77e7fd283c95001521afd6f464
Waleedsaber22 <waleed.sa220@gmail.com>
2025-06-09 04:19:10 +0300

    REM-9-add-steps-storage-handlers-inside-context

--- src/features/steps/contexts/stepsContextProvider/StepsProvider.jsx
+++ src/features/steps/contexts/stepsContextProvider/StepsProvider.jsx
@@ -1,21 +1,46 @@
-import { useReducer } from "react";
+import { useMemo, useReducer } from "react";
 import { StepsContext } from "./StepsContext";
 import { stepsReducer, initialStepState } from "./StepsReducer";
+import { useLocalStorage } from "../../../../hooks/useLocalStorage";
+import { stepsUtils } from "../../utils";
 
 export const StepsProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(stepsReducer, initialStepState);
-
-  const setStepData = (key, value) =>
-    dispatch({ type: "SET_DATA", payload: { key, value } });
+  const [stepsLocalStorage, setStepsLocalStorage] = useLocalStorage(
+    "steps_form_data",
+    stepsUtils.getStepsLocalStoragefromData(initialStepState)
+  );
+  const [state, dispatch] = useReducer(
+    stepsReducer,
+    stepsUtils.mergeStepsLocalStorageIntoData(
+      stepsLocalStorage,
+      initialStepState
+    )
+  );
+  useMemo(() => {
+    state.updateLocalStorage = function () {
+      setStepsLocalStorage(stepsUtils.getStepsLocalStoragefromData(this));
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const setStepData = (key, value, props) =>
+    dispatch({ type: "SET_DATA", payload: { key, value, props } });
 
   const value = {
     getCurrentStep() {
-      return state.steps[state.currentStepKey];
+      const currentStep = state.steps[state.currentStepKey];
+
+      // Bypass `enableNextNove` for all steps except `selectSkip`
+      currentStep.enableNextMove =
+        currentStep.key === "selectSkip" ? !!currentStep.formData : true;
+      return currentStep;
     },
     next: () => dispatch({ type: "NEXT" }),
     prev: () => dispatch({ type: "PREV" }),
     setCurrentStep: (key) => dispatch({ type: "SET_STEP", payload: key }),
     setStepData,
+    setCurrentStepData(value, props) {
+      return setStepData(state.currentStepKey, value, props);
+    },
     reset: () => dispatch({ type: "RESET" }),
   };
 
